598cc466e6bcf810a44e4bc2422f289b8f4b2b92
wwwehab123alsalhy-spec <www.ehab123alsalhy@gmail.com>
2026-02-27 04:05:37 +0300

    Add device configuration for Infinix Hot 30 Play
    $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
    PRODUCT_DEVICE := x6835b
    PRODUCT_NAME := twrp_x6835b
    PRODUCT_BRAND := Infinix
    PRODUCT_MODEL := Infinix Hot 30 Play
    PRODUCT_MANUFACTURER := infinix

---
+++ twrp_x6835b.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+PRODUCT_DEVICE := x6835b
+PRODUCT_NAME := twrp_x6835b
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix Hot 30 Play
+PRODUCT_MANUFACTURER := infinix
