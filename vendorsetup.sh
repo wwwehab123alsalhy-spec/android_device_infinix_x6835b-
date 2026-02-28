f99dd6e122968c5cc230f326823427b7ad2bc285
Ehab <www.ehab123alsalhy@gmail.com>
2026-03-01 02:38:42 +0300

    Add vendor directory creation in vendorsetup.sh
    Create vendor directory for recovery.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
 add_lunch_combo twrp_x6835b-eng
 add_lunch_combo twrp_x6835b-userdebug
 rm -rf device/infinix/x6835b/recovery/root/vendor
+mkdir -p device/infinix/x6835b/recovery/root/vendor
+rm -rf device/infinix/x6835b/recovery/root/vendor
